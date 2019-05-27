b78ba0720f4d2bf0108c452835cb5ab540e138c4
Sean <sjgallen@gmail.com>
2019-05-27 15:56:07 -0500

    added featurs for sales form

--- src/app/sales-account-input-field/sales-account-input-field.component.ts
+++ src/app/sales-account-input-field/sales-account-input-field.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'sales-account-input-field',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalesAccountInputFieldComponent implements OnInit {
 
-  constructor() { }
+   registered = false;
+   submitted = false;
+   userForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) { }
+  
+
+  invalidFirstName() {
+     return (this.submitted && this.userForm.controls.first_name.errors != null);
   }
+  invalidLastName() {
+     return (this.submitted && this.userForm.controls.last_name.errors != null);
+  }
+  invalidCompanyName() {
+     return (this.submitted && this.userForm.controls.company_name.errors != null);
+  }
+  invalidEmail() {
+     return (this.submitted && this.userForm.controls.email.errors != null);
+  }
+  invalidAddress() {
+     return (this.submitted && this.userForm.controls.address.errors != null);
+  }
+  ngOnInit() {
+    this.userForm = this.formBuilder.group({
+         first_name: ['', Validators.required],
+         last_name: ['', Validators.required],
+         company_name: ['', Validators.required],
+         email: ['', Validators.required, Validators.email],
+         address: ['', Validators.required]
+    });
+  }
+  onSubmit()
+  {
+    this.submitted = true;
+ 
+    if(this.userForm.invalid == true){
+      return;
+    }
+    else{
+      this.registered = true;
+    }
+  }
+  
 
 }
