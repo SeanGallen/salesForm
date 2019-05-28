cb9445b3244cc49e8a33eee55aa9aec0dce8002b
Sean <sjgallen@gmail.com>
2019-05-27 19:24:33 -0500

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SalesAccountInputFieldComponent } from './sales-account-input-field/sales-account-input-field.component';
+import { CompanyDetailsDispayComponent } from './company-details-dispay/company-details-dispay.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SalesAccountInputFieldComponent
+  },
+  {
+    path: 'company/:uid',
+    component: CompanyDetailsDispayComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
