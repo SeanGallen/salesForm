e24fdf2dcc32d69d3aa006638ad22e94129fc8b5
Sean <sjgallen@gmail.com>
2019-06-14 22:19:40 -0500

    started routing 4 login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SalesAccountInputFieldComponent } from './component/sales-account-input-field/sales-account-input-field.component';
 import { CompanyDetailsDispayComponent } from './component/company-details-dispay/company-details-dispay.component';
+import { LoginComponent } from './component/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'sales',
     component: SalesAccountInputFieldComponent
   },
   {
