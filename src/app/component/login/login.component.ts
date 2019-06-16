886bbcbec9122e166fd40169f97645969b2f93e2
Sean <sjgallen@gmail.com>
2019-06-16 15:36:07 -0500

    added routing to login

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'login-component',
@@ -10,13 +12,17 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,) { }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
       user_name : [''],
       password: ['']
     });
+  }
+
+  onSubmit(){
+    this.router.navigate(['/sales']);
   }
 
 }
