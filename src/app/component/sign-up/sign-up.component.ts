742a0721e3c1dff2358aae9718677e7593a51e09
Sean <sjgallen@gmail.com>
2019-06-19 22:30:32 -0400

    added more function

--- src/app/component/sign-up/sign-up.component.ts
+++ src/app/component/sign-up/sign-up.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-sign-up',
+  selector: 'sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  signUpForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
 
   ngOnInit() {
+    this.signUpForm = this.formBuilder.group({
+      mobile_number_Email : [''],
+      full_name : [''],
+      user_name : [''],
+      password: ['']
+    })
+
   }
 
 }
