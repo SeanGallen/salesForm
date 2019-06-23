c4b76db3fb85bab09a1ab092f0d79e2e175f350f
Sean <sjgallen@gmail.com>
2019-06-22 21:11:45 -0500

    added link to sign up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { SalesAccountInputFieldComponent } from './component/sales-account-input-field/sales-account-input-field.component';
 import { CompanyDetailsDispayComponent } from './component/company-details-dispay/company-details-dispay.component';
 import { LoginComponent } from './component/login/login.component';
+import { SignUpComponent } from './component/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent
+  },
+  {
+    path: 'signUp',
+    component: SignUpComponent
   },
   {
     path: 'sales',
